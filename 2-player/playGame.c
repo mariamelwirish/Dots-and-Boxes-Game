1fc77d45cf9d2f525acb399f783b835052540e52
Mariam Elwirish <mariamelwirish@gmail.com>
2025-03-03 19:11:54 +0200

    updated playGame.c

--- 2-player/playGame.c
+++ 2-player/playGame.c
@@ -8,6 +8,17 @@ void playGame() {
 	board[0][4] = 1; 
     board[1][4] = 1; 
 	displayBoard(board);
+
+	// I disabled this loop for now but this should be the final result
+	/* int num;
+	do { 
+		printf("Player %c's turn. Enter the row and column of the first dot (e.g., A0 -> 0 0) and second dot: \n", (cur_player == 1 ? 'A' : 'B'));
+		// getPlayerMove()
+		// drawLine()
+		// checkBox()
+		// calculateScores()
+		// switchTurn()
+	} while (!isGameOver()); */
 }
 
 int main() {
