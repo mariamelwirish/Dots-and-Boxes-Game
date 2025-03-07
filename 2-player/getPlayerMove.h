def4a6fbe9f13a9092b7ab2e85ec6b1b97d9e14e
Mariam Elwirish <mariamelwirish@gmail.com>
2025-03-07 23:28:53 +0200

    fixed the headers issue and return type

---
+++ 2-player/getPlayerMove.h
@@ -0,0 +1,10 @@
+#ifndef GET_PLAYER_MOVE_H
+#define GET_PLAYER_MOVE_H
+
+#include "initializeGame.h"  
+#include "helpers.h"
+#include "drawLine.h"
+
+int getPlayerMove(int *r1, int *c1, int *r2, int *c2);
+
+#endif
