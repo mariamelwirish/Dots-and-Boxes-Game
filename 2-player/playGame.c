833061e457fc97891c25af346179c4f2ac6d8039
Mariam Elwirish <mariamelwirish@gmail.com>
2025-03-07 23:24:39 +0200

    edited files to keep up with the headers

--- 2-player/playGame.c
+++ 2-player/playGame.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
-#include "displayBoard.c"
-#include "initializeGame.c"
-#include "drawLine.c"
+#include "playGame.h"
 
 // Test here for now.
 void playGame() {
 	initializeGame();
-	// First 5 tests - static input for now till we develop getPlayerMove()
-	drawLine(0, 4, 0, 3);
-	drawLine(0, 4, 1, 4);
-	drawLine(0, 5, 1, 5);
-	drawLine(1, 3, 1, 4);
-	drawLine(0, 3, 1, 3);
-	displayBoard();
+	
+	int input;
+	do { 
+		displayBoard();
+		printf("Player %c's turn. Enter the row and column of the first dot (e.g., A0 -> 0 0) and second dot: \n", (cur_player == 0 ? 'A' : 'B'));
+		int r1, c1, r2, c2;
+		getPlayerMove(&r1, &c1, &r2, &c2);
+		printf("%d%d%d%d", r1, c1, r2, c2);
+		drawLine(r1, c1, r2, c2);
+		// checkBox();
+		// calculateScores();
+		// switchTurn();
+	} while (1);
+
 	// I disabled this loop for now but this should be the final result
 	/*
 	do { 
@@ -27,6 +32,7 @@ void playGame() {
 			// print error message.
 		}
 	} while (!isGameOver()); */
+	
 }
 
 int main() {
