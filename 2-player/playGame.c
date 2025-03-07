aa5cffa53cf9d3a97bb00b71fe26e939ad41f490
Mariam Elwirish <mariamelwirish@gmail.com>
2025-03-08 00:23:13 +0200

    edited playGame() design

--- 2-player/playGame.c
+++ 2-player/playGame.c
@@ -10,27 +10,19 @@ void playGame() {
 		displayBoard();
 		printf("Player %c's turn. Enter the row and column of the first dot (e.g., A0 -> 0 0) and second dot: \n", (cur_player == 0 ? 'A' : 'B'));
 		int r1, c1, r2, c2;
-		getPlayerMove(&r1, &c1, &r2, &c2);
+		getPlayerMove(&r1, &c1, &r2, &c2); // it calls drawLine() inside it
 		printf("%d%d%d%d", r1, c1, r2, c2);
-		drawLine(r1, c1, r2, c2);
-		// checkBox();
-		// calculateScores();
-		// switchTurn();
+		// calculateScores(); // it calls both checkBox() and switchTurn() inside
 	} while (1);
 
 	// I disabled this loop for now but this should be the final result
 	/*
 	do { 
 		printf("Player %c's turn. Enter the row and column of the first dot (e.g., A0 -> 0 0) and second dot: \n", (cur_player == 0 ? 'A' : 'B'));
-		// int r1, c1, r2, c2;
-		// if(getPlayerMove(&r1, &c1, &r2, &c2)) {
-			// checkBox();
-			// calculateScores();
-			// switchTurn();
-		}
-		else {
-			// print error message.
-		}
+		int r1, c1, r2, c2;
+		getPlayerMove(&r1, &c1, &r2, &c2);
+		printf("%d%d%d%d", r1, c1, r2, c2);
+		calculateScores(); // it calls both checkBox() and switchTurn() inside
 	} while (!isGameOver()); */
 	
 }
