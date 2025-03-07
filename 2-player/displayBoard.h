d5a38af30f5364e57f3e87e3577d319884eea41f
Mariam Elwirish <mariamelwirish@gmail.com>
2025-03-07 23:22:08 +0200

    added a header for each file to work

---
+++ 2-player/displayBoard.h
@@ -0,0 +1,8 @@
+#ifndef DISPLAY_BOARD_H
+#define DISPLAY_BOARD_H
+
+#include "initializeGame.h"
+
+void displayBoard(void);
+
+#endif
---
+++ 2-player/drawLine.h
@@ -0,0 +1,9 @@
+#ifndef DRAW_LINE_H
+#define DRAW_LINE_H
+
+#include "helpers.h"
+
+
+int drawLine(int r1, int c1, int r2, int c2);
+
+#endif
