8b52022c29b5469dd280f23fd7ba7f1f6869b73f
Mariam Elwirish <mariamelwirish@gmail.com>
2025-03-16 02:37:42 +0200

    created function to generate random moves for easyMode

--- 2-player/main.c
+++ 2-player/main.c
@@ -1,9 +1,11 @@
 #include "boardFunctions.c"
 #include "playerFunctions.c"
 #include "driverFunctions.c"
+#include "botFunctions.c"
 
 // main function calls playGame
 int main() {
+	srand((unsigned int) time(NULL)); 
 	// Ask user to select mode and handle it.
 	twoPlayer();
 	
